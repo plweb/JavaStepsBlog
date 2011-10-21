3a2b3ff330d1974cd75c951b97a5f7d295294575
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2011-10-21 15:45:49 +0800

    initial source commit

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/JavaStepsBlog/"
+http_images_path = "/JavaStepsBlog/images"
+http_fonts_path = "/JavaStepsBlog/fonts"
+css_dir = "public/JavaStepsBlog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
